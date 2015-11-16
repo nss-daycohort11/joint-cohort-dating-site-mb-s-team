8ad57f4c157c3555c3ffd4770bd20dbec3f2aa7a
Peter Connolly <pmconnolly80@gmail.com>
2015-11-16 14:56:31 -0600

    auth

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -2,19 +2,23 @@ require.config({
   baseUrl: './javascripts',
   paths: {
     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'firebase': '../lib/bower_components/firebase/firebase',
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
     'q': '../lib/bower_components/q/q',
     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
 require(
-  ["dependencies"], 
-  function(_$_) {
+  ["dependencies", "authentication"], 
+  function(dependencies, auth) {
 
     /*
       You can choose to use the REST methods to interact with
--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -1,4 +1,5 @@
 define(function(require) {
+  var firebase = require("firebase");
   var $ = require("jquery");
   var b = require("bootstrap");
   var h = require("hbs");
