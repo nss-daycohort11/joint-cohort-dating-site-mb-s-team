66d1c39546123ebfedded8c15f2925d2bf2b3162
Peter Connolly <pmconnolly80@gmail.com>
2015-11-16 15:01:33 -0600

    auth-update

--- javascripts/add-favorite.js
+++ javascripts/add-favorite.js
@@ -4,7 +4,7 @@ define(function(require) {
   // This function should return a promise
   function() {
     $.ajax({
-      url: "your favorites Firebase URL here",
+      url: "https://lampgroupproject.firebaseio.com/",
       method: "POST"
     })
     .done(function(response) {
--- javascripts/potential-mates.js
+++ javascripts/potential-mates.js
@@ -4,7 +4,7 @@ define(function(require) {
   // This function should return a promise
   function() {
     $.ajax({
-      url: "your dating Firebase URL here"
+      url: "https://lampgroupproject.firebaseio.com/"
     })
     .done(function(response) {
       // Resolve the promise
